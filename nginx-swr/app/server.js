f8b722098ad3be5f511f8ad0602da874612e31d2
Milan Felix Šulc <rkfelix@gmail.com>
2025-09-15 14:38:50 +0200

    Nginx SWR: add testing tool
    GitOrigin-RevId: 59aff56379cb4a839d01b97c7cfecbf409a74a0a

--- nginx-swr/app/server.js
+++ nginx-swr/app/server.js
@@ -1,8 +1,25 @@
 const server = Bun.serve({
     port: 3000,
     fetch(request) {
+        // Date
         const now = new Date();
-        return new Response(`Now: ${now.toUTCString()}`);
+        const date =
+            String(now.getFullYear()) + '-' +
+            String(now.getMonth() + 1).padStart(2, '0') + '-' +
+            String(now.getDate()).padStart(2, '0') + '-' +
+            String(now.getHours()).padStart(2, '0') + ':' +
+            String(now.getMinutes()).padStart(2, '0') + ':' +
+            String(now.getSeconds()).padStart(2, '0');
+
+        const response = new Response(`Now: ${date}`);
+
+        // Access logs
+        const clientIP = request.headers.get('x-forwarded-for') || request.headers.get('x-real-ip') || 'unknown';
+        const userAgent = request.headers.get('user-agent') || 'unknown';
+
+        console.log(`[${date}] ${request.method} ${request.url} - ${response.status} - IP: ${clientIP} - UA: ${userAgent}`);
+
+        return response;
     },
 });
 
